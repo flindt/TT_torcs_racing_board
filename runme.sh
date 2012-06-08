966907190584f4e026495cfcc862927ed3fef87c
flindt <pfl@eal.dk>
2012-06-08 18:02:09 +0200

    only download if file is not there

--- runme.sh
+++ runme.sh
@@ -8,7 +8,8 @@ sudo apt-get install cvs libalut-dev g++ libgl1-mesa-dev freeglut3-dev libplib-d
 #cvs -z3 -d:pserver:anonymous@torcs.cvs.sourceforge.net:/cvsroot/torcs co -r r1-3-1 -P torcs
 # Or if cvs is blocked by firewall 
 if [ ! -f  torcs-1.3.1.tar.bz2 ]
-wget http://sourceforge.net/projects/torcs/files/all-in-one/1.3.1/torcs-1.3.1.tar.bz2
+then
+	wget http://sourceforge.net/projects/torcs/files/all-in-one/1.3.1/torcs-1.3.1.tar.bz2
 fi
 tar xvf torcs-1.3.1.tar.bz2
 
